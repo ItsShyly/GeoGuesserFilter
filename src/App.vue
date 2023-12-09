4e1a33a11015ab8fd011a79135ef38cc1e028115
Nils Schmidt <nils.schmidt@vinure.de>
2023-12-09 11:56:08 +0100

    Initial Vue setup

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import FilterComponent from './components/FilterComponent.vue'
+
+</script>
+
+<template>
+  <div class="container">
+      <FilterComponent/>
+  </div>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+</style>
